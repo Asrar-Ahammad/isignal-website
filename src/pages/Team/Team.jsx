1d599e1bcd6210cde3bde0b257857dc11de2820f
Asrar-Ahammad <asrarahammadshaik@gmail.com>
2024-01-20 11:56:10 +0530

    Team page update

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,32 +1,189 @@
-import React from 'react';
-import './Team.css';
-import Header from '../../components/header/header';
+import React from "react";
+import "./Team.css";
+import Header from "../../components/header/header";
 import Footer from "../../components/footer/footer";
-import ProductPara from '../../components/ProductPara/ProductPara';
-import img from '../../assets/images/director.png';
+import director from "../../assets/images/director.png";
+import seniorArchitect from "../../assets/images/Lavanya.jpeg";
+import surya from "../../assets/images/Surya.png";
+import viswa from "../../assets/images/viswaraj.png";
 
 function Team() {
-    return (
-        <>
-            <Header />
-            <div className="team-page">
-                <div className="team-container">
-                    <div className="team-img">
-                        <img src={img} alt="Director Image" />
-                        <p>Ex-Infosys, Ex-L&T with several Engagements with Samsung, Intel, Mediatek, Nortel, Nokia with Expertise in 5G RAN Protocols | Trainings & Lab Setups| B5G,6G, mmWave Research |IEEE ComSoc</p>
-                    </div>
-                    <div className="team-info">
-                        <h2>Mr.Ram</h2>
-                        <p><span>Profile:</span><br /><br />
-                            • Chief Mentor, Instructor for iSignal Research Labs, with 15years of Industrial work experience in Cellular Air Interface Technologies for various projects like OAM, Configurations, 4G,5G Protocol Stack implementation for Samsung, L & T Technologies, Infosys, 3G Network Solutions, Rohde & Schwarz for Intel UEs, worked for MediaTek chipsets in 3G WCDMA and 4G LTE OFDMA, MIMO, and Core network for Nortel Client. He is the Active member of various IEEE Societies of Bangalore Chapter, IEEE ComSoc, Signal Processing Society, attended and conducted various IEEE events in the past in Bangalore. He has conducted several Guest Lectures, Invited Tech Talks, Workshops, Seminars and Webinars in the past. Currently he is involved in Next Generation 5G/6G Air Interface Research and Development of Proof of Concepts and Contributions to 3gpp Standards, along with activities like career enablement, Trainings, Lab-Setups and involved in on demand Projects and dedicated to support academia from industry stand point.<br/><br/>
-                            • Research Focus: B5G,6G, Signal processing with AI, ML, DL for the development/creation of the solutions involving MIMO, massiveMIMO, mmWave, beamforming advanced technologies.<br/><br/>
-                            • PoC Focus: SDR Setups, DSP, 4G, 5G EN-DC NR PoC Setups 6G Simulations, B5G. 3gpp Contributions,</p>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <div className="team-page">
+        <div className="team-card">
+          <div className="team-img">
+            <img src={director} alt="" />
+          </div>
+          <div className="team-info">
+            <h2>Mr.Ram</h2>
+            <h3>Cheif Mentor</h3>
+            <h4>Experience</h4>
+            <p className="xp">
+              Ex-Infosys, Ex-L&T with several Engagements with Samsung,
+              Intel,Mediatek, Nortel, Nokia with Expertise in 5G RAN Protocols |
+              Trainings & Lab Setups| B5G,6G, mmWave Research | IEEE ComSoc
+            </p>
+            <h4>Professional work</h4>
+            <p>
+              15-year experienced Chief Mentor at iSignal Research Labs.
+              Extensive expertise in Cellular Air Interface Technologies,
+              including OAM, Configurations, 4G, 5G Protocol Stack
+              implementation for Samsung, L&T, Infosys, MediaTek, Rohde &
+              Schwarz, Nortel.
+            </p>
+            <p>
+              He is the Active member of various IEEE Societies of Bangalore
+              Chapter, IEEE ComSoc, Signal Processing Society, attended and
+              conducted various IEEE events in the past in Bangalore.
+            </p>
+            <p>
+              He has conducted several Guest Lectures, Invited Tech Talks,
+              Workshops, Seminars and Webinars in the past.
+            </p>
+            <p>
+              Currently Engaged in 5G/6G R&D, 3GPP Standards, career enablement,
+              training, lab setups, on-demand projects, and supporting academia
+              from an industry perspective.
+            </p>
+          </div>
+        </div>
+        <div className="team-card">
+          <div className="team-img">
+            <img src={seniorArchitect} alt="" />
+          </div>
+          <div className="team-info">
+            <h2>Lavanya HM</h2>
+            <h3>Senior Architect</h3>
+            <h4>Experience</h4>
+            <p className="xp">
+              Lavanya HM, Software Architect at iSignal Research Labs,
+              Bangalore, with 10+ years in 4G/5G. Expertise spans BTS, OAM, LTE
+              RAN protocols, C/C++, Linux (Ubuntu/Contiki), base station/UE dev.
+              Proficient in IETF RFCs, 6lowPAN (IoT). Engaged with IISc
+              Bangalore, Mindtree, Alcatel Lucent, and 3 years in university
+              faculty roles.
+            </p>
+            <h4>Professional work</h4>
+            <p>
+              <span>Conference Visits:</span>
+              <br /> Contributed to Mindtree Osmosis showcasing LTE Access in
+              2014. Received in-depth LTE training. Organized IoT workshops at
+              IISc (2016) and CDAC, Trivandrum (2017).
+            </p>
+            <p>
+              <span>Research Focus:</span>
+              <br /> 5G, 6G, IoT, AI, ML, Blockchain. Routing Protocols: RPL,
+              AODV, ongoing work on asymmetrical link-based AODV for LLNs.
+              Lavanya H M, Satish A, Charles Perkins, Malati Hegde, Anand S V R.
+            </p>
+            <p>
+              <span>Book Chapter Publications:</span>
+              <br /> ScienceDirect:
+              <a
+                href="https://www.sciencedirect.com/science/article/abs/pii/B9780323991995000161"
+                style={{ color: "blue" }}
+              >
+                {" "}
+                Visit website
+              </a>
+              <br />
+              DoI:{" "}
+              <a
+                style={{ color: "blue" }}
+                href="https://doi.org/10.1016/B978-0-323-99199-5.00016-1"
+              >
+                {" "}
+                Visit website
+              </a>
+              <br />
+              <br />
+              <span>Responsibility:</span>
+              <br />
+              C, Advanced Data Structures, Linux Sys Programming, Algorithm
+              Design for Air Interface, Python for Wireless Automation,
+              Practical C++ on Linux.
+            </p>
+            <p>
+              Engaged in academic research, supporting LTE RAN, 5G/NR RAN - Air
+              Interface Access Protocols for UE and gNB.
+            </p>
+          </div>
+        </div>
+        <div className="team-card">
+          <div className="team-img">
+            <img className="portait-image" src={surya} alt="" />
+          </div>
+          <div className="team-info">
+            <h2>Surya Patar Munda</h2>
+            <h3>Cheif Mentor</h3>
+            <h4>Experience</h4>
+            <p className="xp">
+              Bally Technology | Sasken Networks Engg | Spirent Communacation |
+              Nortel Networks | Motorola | HCL Tech. | ITI Ltd.35+ years
+              experience in wireless telecom industry from 1G to 5G, architected
+              solutions in product development, testing, training, and business
+              development.
+            </p>
+            <h4>Professional work</h4>
+            <p>
+              Technical expertise across 4G/5G standards, protocols, interfaces;
+              LTE enhancements for IoT; 5G system and protocol implementation.
+            </p>
+            <p>
+              Recent work on developing 5G IoT gateway enabling IoT devices as
+              5G compliant for registration, connectivity, QoS requests.
+            </p>
+            <p>
+              Built components for 5G network access, core network, modes,
+              interfaces; expertise in NAS, RRC, MAC, scheduler for 4G/5G
+              systems.
+            </p>
+            <p>
+              Experience with AT commands, VoIP applications, disaster
+              management apps using LTE ProSe services, SIP clients over
+              Asterisk server.
+            </p>
+          </div>
+        </div>
+        <div className="team-card">
+          <div className="team-img">
+            <img src={viswa} alt="" />
+          </div>
+          <div className="team-info">
+            <h2>Mr.Viswaraj</h2>
+            <h3>Research and IP Consultant</h3>
+            <h4>Experience</h4>
+            <p className="xp">
+              Dr. Vishwaraj has over 8 years of experience in academia,
+              research, industry, and Intellectual Property (IP).
+            </p>
+            <h4>Professional work</h4>
+            <p>
+              He has a B.E. degree in Electronics and Communication Engineering
+              and an M.Tech in Digital Communication and Networking.
+            </p>
+            <p>
+              He earned his PhD in Wireless Communication from the Central
+              University of Karnataka (CUK).
+            </p>
+            <p>
+              He is a registered and practicing Patent Agent (IN/PA 5325) with
+              expertise in all aspects of patent processes.
+            </p>
+            <p>
+              His research interests are primarily in Wireless Communication and
+              Signal Processing, and he is an active member of several
+              professional organizations, including the IEEE and the Institute
+              of Engineers (India).
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Team
+export default Team;
---
+++ src/pages/Team/team_details.js
@@ -0,0 +1,73 @@
+export const details = [
+  {
+    photo: "src/assets/images/director.png",
+    photoDesc: `Chief Mentor`,
+    name: `Mr.Ram`,
+    profile1: `
+    Ex-Infosys, Ex-L&T with several Engagements with Samsung, Intel,Mediatek, Nortel, Nokia with Expertise in 5G RAN Protocols | Trainings & Lab Setups| B5G,6G, mmWave Research | IEEE ComSoc
+        • Chief Mentor, Instructor for iSignal Research Labs, with 15years
+        of Industrial work experience in Cellular Air Interface
+        Technologies for various projects like OAM, Configurations, 4G,5G
+        Protocol Stack implementation for Samsung, L & T Technologies,
+        Infosys, 3G Network Solutions, Rohde & Schwarz for Intel UEs,
+        worked for MediaTek chipsets in 3G WCDMA and 4G LTE OFDMA, MIMO,
+        and Core network for Nortel Client. He is the Active member of
+        various IEEE Societies of Bangalore Chapter, IEEE ComSoc, Signal
+        Processing Society, attended and conducted various IEEE events in
+        the past in Bangalore. He has conducted several Guest Lectures,
+        Invited Tech Talks, Workshops, Seminars and Webinars in the past.
+        Currently he is involved in Next Generation 5G/6G Air Interface
+        Research and Development of Proof of Concepts and Contributions to
+        3gpp Standards, along with activities like career enablement,
+        Trainings, Lab-Setups and involved in on demand Projects and
+        dedicated to support academia from industry stand point.`,
+    profile2: `• Research Focus: B5G,6G, Signal processing with AI, ML, DL for
+        the development/creation of the solutions involving MIMO,
+        massiveMIMO, mmWave, beamforming advanced technologies.`,
+    profile3: `• PoC Focus: SDR Setups, DSP, 4G, 5G EN-DC NR PoC Setups 6G
+        Simulations, B5G. 3gpp Contributions,`,
+  },
+  {
+    photo: "src/assets/images/Lavanya.jpeg",
+    photoDesc: `Senior Architect`,
+    name: `Lavanya HM`,
+    profile1: `• Lavanya HM is a Software Architect at iSignal Research Labs, Bangalore. She boasts over 10 years of diverse academic and industry experience in 4G and 5G wireless technologies.
+
+        Throughout her career, Lavanya has held various roles and contributed to crucial programs such as BTS, OAM, and LTE RAN access protocols. Her deep expertise extends to programming languages like C, C++, and Linux platforms like Ubuntu and Contiki OS. This technical proficiency empowers her to develop both base station and UE products.
+        
+        Furthermore, Lavanya possesses a strong understanding of IETF RFCs (4944, 6550, 3561) and 6lowPAN networks (IoT). Her professional journey includes engagements with renowned organizations like IISc Bangalore, Mindtree, and Alcatel Lucent. Notably, she also holds three years of faculty experience through university engagements. 
+
+        • Conference Visits:
+
+        Participated in Mindtree Osmosis and demonstrated LTE Access -
+        2014 
+        Undergone detailed level Training on LTE Access (PHY, MAC, RRC,
+        RLC, PDCP) - 2014 
+        Part of Organizing Team in National level IoT Workshop on IETF
+        conducted in IISc[2016] and CDAC, Trivandrum[2017]
+
+        • Research Focus: 
+
+        1. 5G, 6G, IoT, AI, ML, Block Chain 
+        2. Routing Protocol - RPL, AODV, AODV RPL 
+        3. Ongoing Work: Lavanya H M, Satish A, Charles Perkins, Malati
+        Hegde and Anand S V R, “Asymmetrical link based AODV routing
+        protocol for Low Power and Lossy Networks (LLN)”. 
+        `,
+    profile2: `• Book Chapter Publications:
+        ScienceDirect:
+        <a href="https://www.sciencedirect.com/science/article/abs/pii/B9780323991995000161" style={{color:'blue'}}> Visit website</a>
+        DoI: <a style={{color:'blue'}} href="https://doi.org/10.1016/B978-0-323-99199-5.00016-1"> Visit website</a>
+        <span>Responsibility:</span>
+        • Essential C, Advanced Data Structures
+        • Linux System Programming [Inter Process Communication,
+        Multi-Threading, Socket Programming]
+        • Algorithm Design & Development for Various Air Interface
+        Protocol layers
+        • Essential Python Required for Wireless Automation
+        • Practical C++ on Linux, OOPS, STL, C++11, C++14, C++17.`,
+    profile3: `• Academic Engangements in Research and Support. 
+    • LTE RAN, 5G/NR RAN – Air Interface Access Protocols [PHY, MAC, RLC, PDCP, RRC,
+    SDAP] for Both UE and gNB.`,
+  },
+];
